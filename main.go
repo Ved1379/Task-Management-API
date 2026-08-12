b95ff2b494fe20e65b8c19bba453d0b589b07397
Ved1379 <vedrs1102@gmail.com>
2026-08-12 14:26:06 +0530

    Initial commit

--- main.go
+++ main.go
@@ -33,47 +33,103 @@ func taskHandler(w http.ResponseWriter, r *http.Request) {
 
 	parts := strings.Split(r.URL.Path, "/")
 
-	if r.URL.Path != "/tasks" {
-		id, err := strconv.Atoi(parts[2])
+	if r.URL.Path == "/tasks" {
 
-		if err != nil {
-			fmt.Fprintln(w, "Invalid task Id")
-			return
+		if r.Method == http.MethodGet {
+			for _, task := range tasks {
+				fmt.Fprintln(w, "ID:", task.ID)
+				fmt.Fprintln(w, "Title:", task.Title)
+				fmt.Fprintln(w, "Description:", task.Description)
+			}
 		}
+
+		if r.Method == http.MethodPost {
+
+			var task Task
+
+			err := json.NewDecoder(r.Body).Decode(&task)
+
+			if err != nil {
+				fmt.Fprintln(w, "Invalid JSON")
+				return
+			}
+
+			task.ID = nextID
+			nextID++
+
+			tasks = append(tasks, task)
+
+			fmt.Fprintln(w, "Task created:", task.Title)
+		}
+
+		return
+	}
+
+	id, err := strconv.Atoi(parts[2])
+
+	if err != nil {
+		fmt.Fprintln(w, "Invalid task Id")
+		return
+	}
+
+	if r.Method == http.MethodGet {
+
+		found := false
 
 		for _, task := range tasks {
 			if id == task.ID {
 				json.NewEncoder(w).Encode(task)
+				found = true
 				return
 			}
 		}
-		fmt.Println("Task ID:", id)
-	}
 
-	if r.Method == http.MethodGet {
-		for _, task := range tasks {
-			fmt.Fprintln(w, "ID:", task.ID)
-			fmt.Fprintln(w, "Title:", task.Title)
-			fmt.Fprintln(w, "Description:", task.Description)
+		if !found {
+			fmt.Fprintln(w, "Task not found")
 		}
 	}
 
-	if r.Method == http.MethodPost {
+	if r.Method == http.MethodPut {
 
-		var task Task
+		var updatedTask Task
 
-		err := json.NewDecoder(r.Body).Decode(&task)
+		err := json.NewDecoder(r.Body).Decode(&updatedTask)
 
 		if err != nil {
 			fmt.Fprintln(w, "Invalid JSON")
 			return
 		}
 
-		task.ID = nextID
-		nextID++
+		for i, task := range tasks {
+			if id == task.ID {
 
-		tasks = append(tasks, task)
+				updatedTask.ID = task.ID
+				tasks[i] = updatedTask
 
-		fmt.Fprintln(w, "Task created:", task.Title)
+				json.NewEncoder(w).Encode(updatedTask)
+				return
+			}
+		}
+
+		fmt.Fprintln(w, "Task not found")
+		return
+	} // ← PUT ENDS HERE
+
+	// DELETE GOES HERE
+	if r.Method == http.MethodDelete {
+
+		for i, task := range tasks {
+
+			if id == task.ID {
+
+				tasks = append(tasks[:i], tasks[i+1:]...)
+
+				fmt.Fprintln(w, "Task deleted:", task.Title)
+				return
+			}
+		}
+
+		fmt.Fprintln(w, "Task not found")
+		return
 	}
 }
